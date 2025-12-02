acf073574547f7123de56ed665c77f7562747501
Kashmal Rahat <162123574+kashmal-rahat@users.noreply.github.com>
2025-12-02 13:15:31 +0500

    Day2, Solution 2 Added

---
+++ day2/solution2.py
@@ -0,0 +1,28 @@
+with open('input1.txt', 'r') as f:
+    line = f.read().strip()
+
+def is_invalid(num):
+    s = str(num)
+    n = len(s)
+    for p in range(1, n):
+        if n % p == 0:
+            pattern = s[:p]
+            if pattern * (n // p) == s:
+                return True
+    return False
+
+def find_invalid_digits(sequence_range, invalid_digits):
+    for num in range(sequence_range[0], sequence_range[1] + 1):
+        if is_invalid(num):
+            invalid_digits.append(num)
+
+invalid_digits = []
+for part in line.split(','):
+    start, end = map(int, part.split('-'))
+    find_invalid_digits((start, end), invalid_digits)
+
+print(sum(invalid_digits))
+
+
+
+
